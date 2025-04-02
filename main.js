e22d506b8ed00f2ddc265c96abacf1283b316aea
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-02 14:17:38 +0200

    esercizio svolto

--- main.js
+++ main.js
@@ -11,6 +11,24 @@
 // 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo
 //    faremmo "a mano"
 
+// creo 3 variabili stringa da stampare nelle varie condizioni
+const fizz = "Fizz";
+const buzz = "Buzz";
+const fizzBuzz = "FizzBUzz";
+
+// ciclo for che conta da 1 a 100
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  // se il numero che sta contando è multiplo di 3 e di 5, stampa "FizzBuzz"
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(fizzBuzz);
+    // altrimenti se il numero che sta contando è solo un multiplo di 3 stampa "Fizz"
+  } else if (i % 3 === 0) {
+    console.log(fizz);
+    // altrimenti se il numero che sta contando è solo un multiplo di 5 stampa "Buzz"
+  } else if (i % 5 === 0) {
+    console.log(buzz);
+    // altrimenti stampa semplicemente solo il numero
+  } else {
+    console.log(i);
+  }
 }
